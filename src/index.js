2a387dfc5362420fc77941d573d364861ced51d4
Fabien Champigny <fabien@champigny.name>
2016-10-27 17:52:50 +0200

    add web3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react'
+import Web3 from 'web3'
 
 import _Web3Info from './Web3Info'
 
 export default class Eve extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to Eve React components pack</h2>
-    </div>
-  }
+
+    initWeb3() {
+        if (typeof web3 !== 'undefined') {
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // set the provider you want from Web3.providers
+            window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        }
+    }
+
+    render() {
+        this.initWeb3()
+        const coinbase = web3.eth.coinbase
+        return <div>
+            <h2>Welcome to Eve React components pack</h2>
+            <p>{coinbase}</p>
+        </div>
+    }
 }
 
 export {_Web3Info as Web3Info}
