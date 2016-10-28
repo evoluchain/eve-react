b396e9c4596a7f0d8afa702bd1c67d26b6015857
Fabien Champigny <fabien@champigny.name>
2016-10-28 16:14:28 +0200

    web3 use clean up

--- src/Web3Info.js
+++ src/Web3Info.js
@@ -1,16 +1,9 @@
 import React from 'react'
+import web3 from './web3'
 
 export default class Web3Info extends React.Component {
     render() {
-        const {web3} = this.props
-        let message = ''
-        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-        if (typeof web3 !== 'undefined') {
-            message = 'Version:' + web3.version.api
-        } else {
-            console.log('No web3? You should consider trying Mist/MetaMask!')
-            message =   'No web3? You should consider trying Mist/MetaMask!'
-        }
+        const message = web3.version.api ? web3.version.api : ""
         return (
             <div>
                 <h3>Web3 Info</h3>
