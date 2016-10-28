f590f46308a70de9b7b525d6ac3adbab27a5358a
Fabien Champigny <fabien@champigny.name>
2016-10-28 16:54:05 +0200

    not connected message

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ export default class Eve extends Component {
     constructor() {
         super()
         this.getInfos = this.getInfos.bind(this)
+        this.isNotConnected = this.isNotConnected.bind(this)
         this.state = {
             coinbase: ""
         }
+    }
+
+    componentWillMount(){
         this.getInfos()
     }
 
@@ -21,12 +25,21 @@ export default class Eve extends Component {
         })
     }
 
+    isNotConnected(){
+        if(web3.isConnected()){
+            return false
+        }
+        else {
+            return <dev>Not connected, make sure to use a web3 enebled browser like Mist or Metamask</dev>
+        }
+    }
+
     render() {
         const {coinbase} = this.state
         return <div>
             <h2>Welcome to Eve React components pack</h2>
             <p>{coinbase}</p>
-            {this.props.children}
+            {this.isNotConnected() || this.props.children}
         </div>
     }
 }
