ab792cfc018a2a3ae350f1138872a00a9ba52cb1
Fabien Champigny <fabien@champigny.name>
2016-11-02 19:29:14 +0100

    load account's balance

--- src/Balance.js
+++ src/Balance.js
@@ -1,14 +1,50 @@
 import React from "react";
+import web3 from './web3'
 
 export default class Balance extends React.Component {
     constructor() {
         super();
+        this.state = {
+            balance: null
+        }
+    }
+
+    componentDidMount(){
+        this.getBalance.apply(this, [this.props.account])
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.getBalance.apply(this, [nextProps.account])
+    }
+
+    getBalance(account) {
+        if(!account) {
+            return
+        }
+
+        const balancePromise = new Promise((resolve, reject) => {
+            web3.eth.getBalance(account, (error, result) => {
+                if (error) {
+                    return reject(error)
+                }
+                resolve(result.valueOf())
+            })
+        })
+
+        balancePromise.then(balance => {
+            this.setState({
+                balance
+            })
+        }, _error => {
+            console.error(_error)
+        })
+
     }
 
     render() {
-        const {balance} = this.props;
+        const {balance} = this.state
         return (
-            <div>{balance}</div>
+            <span>{balance}</span>
         );
     }
 }
