02d5b7cdd5622ac0c07755932a40396cf7105735
Fabien Champigny <fabien@champigny.name>
2016-11-02 15:52:59 +0100

    getInfos es6 style

--- src/Web3Info.js
+++ src/Web3Info.js
@@ -5,7 +5,7 @@ export default class Web3Info extends React.Component {
 
     constructor() {
         super()
-        this.getInfos = this.getInfos.bind(this)
+        //this.getInfos = this.getInfos.bind(this)
         this.state = {
             apiVersion: "",
             nodeVersion: "",
@@ -16,22 +16,45 @@ export default class Web3Info extends React.Component {
     }
 
     getInfos() {
-        //TODO : make it the ES6 way
-        var that = this
-        web3.eth.getCoinbase(function (error, result) {
-            that.setState({coinbase: result})
+        const coinbasePromise = new Promise((resolve, reject) => {
+            web3.eth.getCoinbase((error, result) => {
+                if (error) {
+                    return reject(error)
+                }
+                resolve(result)
+            })
         })
-        web3.version.getNode(function (error, result) {
-            that.setState({nodeVersion: result})
+        const nodePromise = new Promise((resolve, reject) => {
+            web3.version.getNode((error, result) => {
+                if (error) {
+                    return reject(error)
+                }
+                resolve(result)
+            })
         })
-        that.setState({apiVersion: web3.version.api})
-        web3.version.getNetwork(function (error, result) {
-            that.setState({networkVersion: result})
+        const networkPromise = new Promise((resolve, reject) => {
+            web3.version.getNetwork((error, result) => {
+                if (error) {
+                    return reject(error)
+                }
+                resolve(result)
+            })
         })
+
+        const allReady = Promise.all([coinbasePromise, nodePromise, networkPromise])
+        allReady.then(results => {
+            const [ coinbase, nodeVersion, networkVersion ] = results
+            this.setState({
+                coinbase,
+                nodeVersion,
+                networkVersion,
+                apiVersion : web3.version.api
+            })
+        }, _error => { console.error(_error) })
     }
 
     componentWillMount() {
-        this.getInfos()
+        this.getInfos.apply(this)
     }
 
     render() {
