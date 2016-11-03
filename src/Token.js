1b961238b90096a55a8c2e84a2cbd6e044a9d9cd
Fabien Champigny <fabien@champigny.name>
2016-11-03 14:39:03 +0100

    code clean up

--- src/Token.js
+++ src/Token.js
@@ -8,7 +8,6 @@ export default class Token extends React.Component {
             contract: null,
             balance: null
         }
-        // console.log('constructor state', this.state)
     }
 
     static defaultProps = {
@@ -17,78 +16,35 @@ export default class Token extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        //console.log('componentWillReceiveProps nextProps', nextProps)
-        //console.log('componentWillReceiveProps props', this.props)
-
         this.getToken.bind(this)(nextProps, this.getBalance.bind(this))
-
-
-        //console.log('componentWillReceiveProps state', this.state)
-    }
-
-    componentWillMount() {
-        //console.log('componentWillMount props', this.props)
-        //console.log('componentWillMount state', this.state)
-        //this.getBalance.apply(this)
-
-       // this.setState({toto: 'TOTO'})
-        //console.log('componentWillMount state', this.state)
     }
 
     getToken(props, callback) {
         if (props.address) {
-
-            //console.log('getToken address', address)
-
             // initiate contract for an address
             const that = this
             tokenContract.at(props.address, function (error, contract) {
-
-                //console.log('getToken at', error , contract)
-
                 that.setState({
                     contract: contract
                 })
-
                 callback(contract, props.account)
-                //console.log('getToken state', that.state)
             })
-
-            //this.getBalance.apply(this)
-
         }
     }
 
     getBalance(contract, account) {
-        // console.log('getBalance props', this.props)
-        //console.log('getBalance state', contract, this.state)
-
-        // const {contract} = this.state
-        //const {account} = this.props
-        if (!contract || !account) {
-            console.log()
-            return
-        }
-
-        //console.log('will call getBalance')
-        console.log('this', this)
-        const that = this
-        contract.balanceOf.call(account, function (error, balance) {
-            console.log('contract.balanceOf', balance.valueOf())
-            that.setState({
-                balance: balance.valueOf()
+        if (contract && account) {
+            const that = this
+            contract.balanceOf.call(account, function (error, balance) {
+                that.setState({
+                    balance: balance.valueOf()
+                })
             })
-            console.log('this ???', this)
-        })
-        //console.log('called getBalance')
+        }
     }
 
     render() {
-
-        console.log('render props', this.props)
-        console.log('render state', this.state)
-
-        const {contract, balance} = this.state
+        const {balance} = this.state
         const {address, account} = this.props
         return (
             <span>address:{address} - account:{account} - balance:{balance}</span>
