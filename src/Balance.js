df64166cc58d4d976d464855fa185dc25fddd7e7
Fabien Champigny <fabien@champigny.name>
2016-11-10 02:56:44 +0100

    use Web3Eth

--- src/Balance.js
+++ src/Balance.js
@@ -1,56 +1,22 @@
 import React from "react";
 import web3 from './web3'
+import Web3Eth from './Web3Eth'
 
 export default class Balance extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            balance: null
-        }
-    }
 
     static defaultProps = {
-        account: null,
         unit: 'ether'
     }
 
-    componentDidMount() {
-        this.getBalance.apply(this, [this.props.account])
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.getBalance.apply(this, [nextProps.account])
-    }
-
-    getBalance(account) {
-        if (!account) {
-            return
-        }
-
-        const balancePromise = new Promise((resolve, reject) => {
-            web3.eth.getBalance(account, (error, result) => {
-                if (error) {
-                    return reject(error)
-                }
-                resolve(result.valueOf())
-            })
-        })
-
-        balancePromise.then(balance => {
-            this.setState({
-                balance
-            })
-        }, _error => {
-            console.error(_error)
-        })
-
-    }
-
     render() {
-        const {balance} = this.state
-        const {unit} = this.props
+        const {account, unit} = this.props
         return (
-            <span>{web3.fromWei(balance, unit)} {unit}</span>
+            <Web3Eth method='balance' address={account}>
+                {({error, result}) => {
+                    const balance = result ? web3.fromWei(result.valueOf(), unit) : 'null'
+                    return <span>balance(eth) : {balance} {unit}</span>
+                }}
+            </Web3Eth>
         );
     }
 }
